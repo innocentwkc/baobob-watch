cdff2ccf694797ebd7b157a741cb843e74cfb0ec
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-11-12 00:52:05 +0200

    ping multiple tcp ports

--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
-// Import the tcpPing function using ES6 syntax
+// index.js
+import express from 'express';
 import { tcpPing } from './lib/tools/ping.js';
 
-// Use the tcpPing function
-tcpPing('example.com', 80, 3000, 5); // Ping example.com 5 times
-tcpPing('192.168.1.1', 80, 5000, -1); // Ping 192.168.1.1 indefinitely
+const app = express();
+app.use(express.json());
+
+app.get('/ping', (req, res) => {
+    const { host, ports = '80', timeout, cycles } = req.query; // Default to port 80 if ports not provided
+
+    // Parsing multiple ports from the query parameter
+    const parsedPorts = ports.split(',').map(port => parseInt(port.trim()));
+
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    
+    const emitter = tcpPing(host, parsedPorts, parseInt(timeout), parseInt(cycles));
+    emitter.on('data', (data) => {
+        res.write(`data: ${data}\n\n`);
+    });
+
+    emitter.on('complete', (message) => {
+        res.write(`data: ${message}\n\n`);
+        res.end();
+    });
+});
+
+const port = 4000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
--- server/lib/tools/ping.js
+++ server/lib/tools/ping.js
@@ -1,6 +1,7 @@
-// Import statements using ES6 syntax
+// ping.js
 import net from 'net';
 import { performance } from 'perf_hooks';
+import EventEmitter from 'events';
 
 /**
  * Function to perform a TCP "ping" to a given host and port, with a specified timeout, for a number of cycles.
@@ -9,44 +10,44 @@ import { performance } from 'perf_hooks';
  * @param {number} timeout - The timeout in milliseconds.
  * @param {number} cycles - The number of ping cycles. If set to -1, it will ping indefinitely.
  */
-export function tcpPing(host, port = 80, timeout = 5000, cycles = 4) {
+export function tcpPing(host, ports = [80], timeout = 5000, cycles = 4) {
     let cycleCount = 0;
+    const emitter = new EventEmitter();
 
     const pingCycle = () => {
-        if (cycles !== -1 && cycleCount >= cycles) {
-            console.log(`Completed ${cycles} ping cycles to ${host}:${port}`);
+        if (cycles !== -1 && cycleCount >= cycles * ports.length) {
+            emitter.emit('complete', `Completed ${cycles} ping cycles to ${host} on ports ${ports.join(', ')}`);
             return;
         }
 
+        const currentPort = ports[cycleCount % ports.length];
         cycleCount++;
         const startTime = performance.now();
-        
         const socket = new net.Socket();
-
-        // Set a timeout for the connection attempt
         socket.setTimeout(timeout);
 
         socket.on('connect', () => {
             const endTime = performance.now();
-            console.log(`TCP Ping #${cycleCount} to ${host}:${port} successful. Time: ${Math.round(endTime - startTime)}ms`);
+            emitter.emit('data', `TCP Ping #${cycleCount} to ${host}:${currentPort} successful. Time: ${Math.round(endTime - startTime)}ms`);
             socket.destroy();
-            setTimeout(pingCycle, 1000); // Wait for 1 second before the next cycle
+            setTimeout(pingCycle, 1000);
         });
 
         socket.on('timeout', () => {
-            console.error(`TCP Ping #${cycleCount} to ${host}:${port} timed out.`);
+            emitter.emit('data', `TCP Ping #${cycleCount} to ${host}:${currentPort} timed out.`);
             socket.destroy();
             setTimeout(pingCycle, 1000);
         });
 
         socket.on('error', (error) => {
-            console.error(`TCP Ping #${cycleCount} to ${host}:${port} failed: ${error.message}`);
+            emitter.emit('data', `TCP Ping #${cycleCount} to ${host}:${currentPort} failed: ${error.message}`);
             socket.destroy();
             setTimeout(pingCycle, 1000);
         });
 
-        socket.connect(port, host);
+        socket.connect(currentPort, host);
     };
 
     pingCycle();
+    return emitter;
 }
