66c0ac434f3693459048cbd42a0f8ce83007a426
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-11-12 02:16:08 +0200

    create icmp ping path

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // index.js
 import express from 'express';
-import { tcpPing } from './lib/tools/ping.js';
+import { tcpPing } from './lib/tools/tcpPing.js';
+import icmpPing from './lib/tools/icmpPing.js';
 
 /**
  * Creates an Express application and sets up a route for performing TCP pings.
@@ -15,7 +16,7 @@ app.use(express.json());
  * @param {express.Request} req - The Express request object, containing query parameters.
  * @param {express.Response} res - The Express response object, used to send back the ping results.
  */
-app.get('/ping', (req, res) => {
+app.get('/tcp-ping', (req, res) => {
   // Extracting query parameters and setting default values
   const { host, ports = '80', timeout, cycles } = req.query;
 
@@ -37,6 +38,36 @@ app.get('/ping', (req, res) => {
   emitter.on('complete', (message) => {
     res.write(`data: ${message}\n\n`);
     res.end();
+  });
+});
+
+app.get('/icmp-ping', (req, res) => {
+  const { host, timeout, count } = req.query;
+
+  const parsedTimeout = parseInt(timeout) || 5000;
+  const parsedCount = count === 'infinite' ? -1 : parseInt(count) || 4;
+
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  const emitter = icmpPing(host, parsedTimeout, parsedCount);
+  emitter.on('data', (data) => {
+    res.write(`data: ${data}\n\n`);
+  });
+
+  emitter.on('complete', (message) => {
+    res.write(`data: ${message}\n\n`); 
+    res.end();
+  });
+
+  emitter.on('error', (error) => {
+    res.write(`data: Error: ${error.message}\n\n`);
+    res.end();
+  });
+
+  emitter.on('timeout', (timeoutMessage) => {
+    res.write(`data: ${timeoutMessage}\n\n`);
   });
 });
 
---
+++ server/lib/tools/icmpPing.js
@@ -0,0 +1,49 @@
+// icmpPing.js
+import raw from 'raw-socket';
+import { performance } from 'perf_hooks';
+import EventEmitter from 'events';
+
+function icmpPing(host, timeout = 5000, count = -1) {
+  const socket = raw.createSocket({ protocol: raw.Protocol.ICMP });
+  const emitter = new EventEmitter();
+  let sentCount = 0;
+  let isPinging = true;
+  let startTimes = {};
+
+  socket.on('message', (buffer, source) => {
+    if (source === host && isPinging) {
+      const endTime = performance.now();
+      const startTime = startTimes[sentCount];
+      const time = endTime - startTime;
+
+      emitter.emit('data', `Ping to ${host}: Sequence ${sentCount + 1}, Time: ${time.toFixed(2)}ms`);
+      sentCount++;
+      
+      if (count !== -1 && sentCount >= count) {
+        isPinging = false;
+        socket.close();
+        emitter.emit('complete', 'Ping process completed.');
+      }
+    }
+  });
+
+  const ping = () => {
+    if (!isPinging) return;
+
+    const buffer = Buffer.from([0x08, 0x00, 0x7d, 0x3b, 0x00, 0x00, 0x00, 0x00]);
+    startTimes[sentCount] = performance.now();
+    socket.send(buffer, 0, buffer.length, host, (err) => {
+      if (err) {
+        emitter.emit('error', err);
+      }
+    });
+
+    setTimeout(ping, timeout);
+  };
+
+  ping();
+
+  return emitter;
+}
+
+export default icmpPing;
