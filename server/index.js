ebd408f357d63d9883687af695ee7c16382a4b77
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-11-12 02:36:26 +0200

    add comments

--- server/index.js
+++ server/index.js
@@ -41,31 +41,52 @@ app.get('/tcp-ping', (req, res) => {
   });
 });
 
+/**
+ * API route for performing an ICMP ping to a specified host.
+ * This route accepts query parameters for 'host', 'timeout', and 'count'.
+ * 
+ * @param {express.Request} req - The Express request object, containing query parameters:
+ *   - host (string): The IP address or hostname to be pinged.
+ *   - timeout (string): The interval between each ping in milliseconds. Defaults to 5000ms.
+ *   - count (string): The number of pings to send. Can be set to 'infinite' for indefinite pinging or a specific number. Defaults to 4.
+ * @param {express.Response} res - The Express response object, used to send back the ping results.
+ */
+// Handler for the '/icmp-ping' route
 app.get('/icmp-ping', (req, res) => {
+  // Extract parameters from the query string
   const { host, timeout, count } = req.query;
 
+  // Parse the timeout parameter, default to 5000 milliseconds if not provided or invalid
   const parsedTimeout = parseInt(timeout) || 5000;
+  // Parse the count parameter. Use -1 for infinite pings if 'infinite' is specified, otherwise default to 4
   const parsedCount = count === 'infinite' ? -1 : parseInt(count) || 4;
 
+  // Set response headers for an event stream format
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
 
+  // Invoke the icmpPing function and set up event listeners
   const emitter = icmpPing(host, parsedTimeout, parsedCount);
+
+  // On receiving ping data, send it to the client
   emitter.on('data', (data) => {
     res.write(`data: ${data}\n\n`);
   });
 
+  // On ping completion, send the completion message and close the connection
   emitter.on('complete', (message) => {
     res.write(`data: ${message}\n\n`); 
     res.end();
   });
 
+  // On ping error, send the error message and close the connection
   emitter.on('error', (error) => {
     res.write(`data: Error: ${error.message}\n\n`);
     res.end();
   });
 
+  // On timeout, send the timeout message to the client
   emitter.on('timeout', (timeoutMessage) => {
     res.write(`data: ${timeoutMessage}\n\n`);
   });
--- server/lib/tools/icmpPing.js
+++ server/lib/tools/icmpPing.js
@@ -3,49 +3,65 @@ import raw from 'raw-socket';
 import { performance } from 'perf_hooks';
 import EventEmitter from 'events';
 
+/**
+ * Performs an ICMP (Internet Control Message Protocol) ping to a specified host.
+ * 
+ * @param {string} host - The IP address or hostname to be pinged.
+ * @param {number} [timeout=5000] - The interval between each ping in milliseconds. Defaults to 5000ms.
+ * @param {number} [count=-1] - The number of pings to send. Set to -1 for indefinite pinging. Defaults to -1.
+ * @returns {EventEmitter} - An EventEmitter instance that emits 'data' events for each ping result and a 'complete' event when the ping process is done.
+ */
 function icmpPing(host, timeout = 5000, count = -1) {
+  // Create a raw socket for ICMP protocol
   const socket = raw.createSocket({ protocol: raw.Protocol.ICMP });
   const emitter = new EventEmitter();
-  let sentCount = 0;
-  let isPinging = true;
-  let startTimes = new Map();
+  let sentCount = 0; // Counter for sent pings
+  let isPinging = true; // Flag to control the ping process
+  let startTimes = new Map(); // Map to store start times of pings
 
+  // Handle incoming ICMP messages
   socket.on('message', (buffer, source) => {
     if (source === host && isPinging) {
       const endTime = performance.now();
       const startTime = startTimes.get(sentCount);
-      const time = endTime - startTime;
+      const time = endTime - startTime; // Calculate round trip time
+      // Emit data event for each received ping response
       emitter.emit('data', `Ping to ${host}: Sequence ${sentCount + 1}, Time: ${time.toFixed(2)}ms`);
       sentCount++;
       
+      // Check if the desired number of pings have been sent
       if (count !== -1 && sentCount >= count) {
-        isPinging = false;
-        socket.close();
-        emitter.emit('complete', 'Ping process completed.');
-        startTimes.clear();
+        isPinging = false; // Stop pinging
+        socket.close(); // Close the socket
+        emitter.emit('complete', 'Ping process completed.'); // Emit complete event
+        startTimes.clear(); // Clear the start times map
       }
     }
   });
 
+  // Function to send a ping
   const ping = () => {
-    if (!isPinging) return;
+    if (!isPinging) return; // Stop pinging if flag is false
 
+    // Create an ICMP echo request packet
     const buffer = Buffer.from([0x08, 0x00, 0x7d, 0x3b, 0x00, 0x00, 0x00, 0x00]);
-    startTimes.set(sentCount, performance.now());
+    startTimes.set(sentCount, performance.now()); // Store start time
+    // Send the ICMP packet
     socket.send(buffer, 0, buffer.length, host, (err) => {
       if (err) {
-        emitter.emit('error', err);
+        emitter.emit('error', err); // Emit error event in case of an error
       }
     });
 
+    // Schedule next ping based on timeout
     if (count === -1 || sentCount < count - 1) {
       setTimeout(ping, timeout);
     }
   };
 
-  ping();
+  ping(); // Start the ping process
 
-  return emitter;
+  return emitter; // Return the EventEmitter for event handling
 }
 
 export default icmpPing;
