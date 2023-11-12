69e7fbe97fd3fcde9839c310c9345b33c80e9a82
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-11-12 02:19:52 +0200

    Fix timing calculation in ICMP ping script
    - Refactor the icmpPing function to use a Map for storing start times of individual pings.
    - Ensure accurate round-trip time measurement by matching each ping response with its corresponding start time.
    - Clear the start times Map after the ping process is completed to optimize resource usage.
    - Adjust the ping scheduling logic to handle both finite and infinite ping counts more efficiently.

--- server/lib/tools/icmpPing.js
+++ server/lib/tools/icmpPing.js
@@ -8,14 +8,13 @@ function icmpPing(host, timeout = 5000, count = -1) {
   const emitter = new EventEmitter();
   let sentCount = 0;
   let isPinging = true;
-  let startTimes = {};
+  let startTimes = new Map();
 
   socket.on('message', (buffer, source) => {
     if (source === host && isPinging) {
       const endTime = performance.now();
-      const startTime = startTimes[sentCount];
+      const startTime = startTimes.get(sentCount);
       const time = endTime - startTime;
-
       emitter.emit('data', `Ping to ${host}: Sequence ${sentCount + 1}, Time: ${time.toFixed(2)}ms`);
       sentCount++;
       
@@ -23,6 +22,7 @@ function icmpPing(host, timeout = 5000, count = -1) {
         isPinging = false;
         socket.close();
         emitter.emit('complete', 'Ping process completed.');
+        startTimes.clear();
       }
     }
   });
@@ -31,14 +31,16 @@ function icmpPing(host, timeout = 5000, count = -1) {
     if (!isPinging) return;
 
     const buffer = Buffer.from([0x08, 0x00, 0x7d, 0x3b, 0x00, 0x00, 0x00, 0x00]);
-    startTimes[sentCount] = performance.now();
+    startTimes.set(sentCount, performance.now());
     socket.send(buffer, 0, buffer.length, host, (err) => {
       if (err) {
         emitter.emit('error', err);
       }
     });
 
-    setTimeout(ping, timeout);
+    if (count === -1 || sentCount < count - 1) {
+      setTimeout(ping, timeout);
+    }
   };
 
   ping();
