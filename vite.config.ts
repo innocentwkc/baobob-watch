926caf2719fc555061cd38ce24b6a0b7c6f6b5ee
Innocent WK Chinyemba <innocentwkc@gmail.com>
2023-12-02 01:07:39 +0200

    update dev scripts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4001, // Use port 5000 for the web app frontend
+  },
+   build: {
+    outDir: 'dist', // Specify the output directory
+  },
   plugins: [
     vue(),
   ],
