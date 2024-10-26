9c52d2a6119490d094d5052bb3e766af0e0fb5da
Innocent WK Chinyemba <innocentwkc@gmail.com>
2024-10-26 11:50:40 +0200

    streamline modules

--- scripts/start.js
+++ scripts/start.js
@@ -3,7 +3,10 @@ const { execSync } = require("child_process");
 const tool = process.argv[2] || "all";
 
 if (tool === "all") {
-  execSync('concurrently "npm run start:ping" "npm run start:traceroute"', {
+  // execSync('concurrently "npm run start:ping" "npm run start:traceroute"', {
+  //   stdio: "inherit",
+  // });
+  execSync('concurrently "npm run start:ping"', {
     stdio: "inherit",
   });
 } else {
